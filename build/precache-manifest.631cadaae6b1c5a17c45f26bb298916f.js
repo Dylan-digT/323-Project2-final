1312f14799af7ce5d2972a737c3043be91bf93ed
Dylan-digT <72691293+Dylan-digT@users.noreply.github.com>
2020-11-23 14:31:11 +0200

    static css images

---
+++ build/precache-manifest.631cadaae6b1c5a17c45f26bb298916f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4bd619a3d3826323b3f8e50b0aeabd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb5609d51a686de50531",
+    "url": "/static/css/2.9c82a2bb.chunk.css"
+  },
+  {
+    "revision": "8d63042730e9609adf4a",
+    "url": "/static/css/main.482efe81.chunk.css"
+  },
+  {
+    "revision": "fb5609d51a686de50531",
+    "url": "/static/js/2.d506f0df.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.d506f0df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d63042730e9609adf4a",
+    "url": "/static/js/main.345fac41.chunk.js"
+  },
+  {
+    "revision": "e1fcef772f1fdaabae77",
+    "url": "/static/js/runtime-main.466e3532.js"
+  }
+]);
